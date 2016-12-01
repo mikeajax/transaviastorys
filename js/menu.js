e5642fd833826780cb0ed38c354741b619983c9c
Mike Lam <mikeajax_1994@hotmail.com>
2016-12-01 16:39:36 +0100

    new pages with css

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+/*============================
+=            menu            =
+============================*/
+
+
+$( "#menu" ).click(function() {
+  $( "aside" ).toggleClass( "active" );
+  $( "#menu" ).toggleClass( "active" );
+  $( "body" ).toggleClass( "noscroll" );
+});
+
+
+/*=====  End of menu  ======*/
